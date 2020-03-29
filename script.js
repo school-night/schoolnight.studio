226548526c3c1d46c09caa9818fa4f20bd5fec2b
Ali Torbati <ali.torbati@gmail.com>
2020-03-29 16:31:03 -0700

    remove parens

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const day = new Date().getDay();
 const isSchoolnight = day < 5; // less than friday
-const string = `it ${isSchoolnight ? "is" : "is (not)"} a school night`;
+const string = `${isSchoolnight ? "it’s" : "it’s not"} a school night`;
 const messageElement = document.getElementById("message");
 messageElement.innerHTML = string;
